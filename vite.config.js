89c07a39b933389971c3befda6634f50ff726fce
omjha10-02 <pkpikachu11@gmail.com>
2024-12-11 11:51:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,30 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://survey-app-nous.onrender.com', // Backend URL
+//         changeOrigin: true,
+//         secure: true,
+//       },
+//     },
+//   },
+// });
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://survey-app-nous.onrender.com', // Backend URL
-        changeOrigin: true,
-        secure: true,
-      },
+  server:{
+    proxy:{
+       '/api':'https://survey-app-nous.onrender.com'
+    
     },
   },
-});
+  plugins: [react()],
+})
