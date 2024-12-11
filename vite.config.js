fff2f6dd21d484d9419a2fba01b8093711e79948
omjha10-02 <pkpikachu11@gmail.com>
2024-12-11 12:17:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://survey-app-nous.onrender.com', // Backend URL
-        changeOrigin: true,
-        secure: true,
-      },
+ server: {
+  proxy: {
+    '/api': {
+      target: 'https://survey-app-nous.onrender.com', // Replace with your backend's local URL
+      changeOrigin: true,
     },
   },
+},
+
 });
 
 
